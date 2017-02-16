4d9a1d9fcc7b8b87bfda9d1d6ab54a28f4c216ad
Zach-Haddad <Zhaddad7@gmail.com>
2017-02-16 13:29:46 -0800

    footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => (
   <div className='footer'>
     <div className='footer-logo'></div>
     <div className='footer-links'>
-      <a href='https://github.com/Zach-Haddad/trekmates'>Github</a>
-      <a>Portfolio</a>
-      <a>Credits</a>
+      <a href='https://github.com/Zach-Haddad/trekmates' target="_blank"><i className="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+      <a href='https://linkedin.com/in/zach-haddad' target="_blank"><i className="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+      <a href='https://zach-haddad.com' target="_blank"><i className="fa fa-folder fa-3x" aria-hidden="true"></i></a>
     </div>
   </div>
   );
