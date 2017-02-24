1f6b78592234230b02985572e6f0e88f41d0c9c8
Zach-Haddad <Zhaddad7@gmail.com>
2017-02-24 09:53:33 -0800

    fix minification warning

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var path = require("path");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
@@ -7,6 +8,13 @@ module.exports = {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [
       {
@@ -23,4 +31,5 @@ module.exports = {
   resolve: {
     extensions: ["", ".js", ".jsx" ]
   }
+
 };
