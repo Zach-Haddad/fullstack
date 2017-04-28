bd8a473254c248b255bfdbd9b2f4c670f6a09156
Zach-Haddad <Zhaddad7@gmail.com>
2017-04-27 19:19:26 -0700

    fixed routing error on signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resources :user, only: [:create, :show, :update, :index]
+    resources :users, only: [:create, :show, :update, :index]
     resource :session, only: [:create, :destroy]
     resources :groups, only: [:create, :destroy, :show, :index, :update] do
       resources :events, only: [:create]
